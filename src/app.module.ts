807e99e9ae0d81dcacf94c0aad0fe8e0bf42431e
Chukwudumebi U <79149164+chukwudumebiughonu@users.noreply.github.com>
2022-01-29 14:32:01 +0100

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,23 @@
+
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } frmom '/@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ComponentsModule } from './components/components.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(),DatabaseModule,
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
     GraphQLModule.forRoot({
-    playground: true,
+      playground: true,
       debug: true,
       autoSchemaFile: true,
-   })],
+    }),
+    ComponentsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
